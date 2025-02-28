219a1236c88d805f66557e4853bbbe6a60bc711a
jai099 <jaipatel0903@gmail.com>
2025-02-28 11:26:50 +0530

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from 'react'
+import { Routes, Route } from "react-router"
+import { About } from "./components/About/About"
+import { Login } from "./components/Auth/Login"
+import { Home } from "./components/Home/Home"
+import { Register } from "./components/Auth/Register"
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+import { useDispatch } from 'react-redux'
+import { IsLogin } from './actions/userActions'
+import { Document } from './components/Document'
+import  CollegeLogin from './components/CollegeLogin'
+import { Navbar } from './components/Navbar'
+import KnowYourStatus from './components/KnowYourStatus'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const LogOrNot = () => {
+      dispatch(IsLogin());
+    }
+    LogOrNot()
+  }, []);
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+
+    <Navbar />
+
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/student-register" element={<Register />} />
+        <Route path="/college-login" element={<CollegeLogin />} />
+        <Route path="/document" element={<Document />} />
+        <Route path="/know-your-status" element={<KnowYourStatus />} />
+      </Routes>
+
+
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        className="mt-14 font-bold  "
+
+      />
+
     </>
   )
 }
